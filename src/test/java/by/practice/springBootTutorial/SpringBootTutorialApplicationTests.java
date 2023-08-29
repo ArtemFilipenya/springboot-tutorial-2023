f476fc0f13b0bffcfe8f6e28e0642a3e65c89b60
Artem Filipenya <as.filipenya@gmail.com>
2023-08-29 11:40:39 +0300

    First commit

---
+++ src/test/java/by/practice/springBootTutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package by.practice.springBootTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
